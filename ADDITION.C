6f16e0a2106967db69ab62401d68107ca9321ef0
Srujanaav29 <srujanaav.eng@gmail.com>
2026-05-16 12:28:21 +0530

    adding 2 numbers only
    This program is just used to add two numbers .

---
+++ ADDITION.C
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+printf("Name: SRUJANA.A.V\n");
+printf("USN: 1JS25CI089\n");
+printf("Program: Simple Addition of Two Numbers\n");
+int a, b, sum;
+printf("Enter two numbers:");
+scanf("%d %d", &a, &b);
+sum = a + b;
+printf("Sum = %d\n", sum);
+return 0;
+}
